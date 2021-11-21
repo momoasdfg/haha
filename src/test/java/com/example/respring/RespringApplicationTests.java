b4be4d3265877d1a34c650a688d77be2348ae3c0
Momo <xiaozijava@gmail.com>
2021-11-21 16:45:26 +0800

    noting

---
+++ src/test/java/com/example/respring/RespringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.respring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RespringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
